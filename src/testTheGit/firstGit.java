1405d81adabc7beb9dd5107ab40d934bb2518fdd
ramchaulagain625 <ramchaulagain625@gmail.com>
2021-03-11 20:29:21 -0500

    New class added with opackage

---
+++ src/testTheGit/firstGit.java
@@ -0,0 +1,17 @@
+package testTheGit;
+
+public class firstGit {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		int add =x+y;
+		int multi =x*y;
+		int div =y/x;
+		int sun =y-x;
+		System.out.println(add);
+		System.out.println("Learning git very slow");
+
+	}
+
+}
